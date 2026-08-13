3b592b0d2b3305164b5eacd187382a508f34ed55
ivLis <me@ivl.is>
2026-08-14 00:38:04 +0900

    fix: hide navigation bar in portrait mode

--- app/src/main/java/kr/ivlis/ivlyricsandroid/MainActivity.java
+++ app/src/main/java/kr/ivlis/ivlyricsandroid/MainActivity.java
@@ -3090,11 +3090,18 @@ public final class MainActivity extends Activity implements
             }
         } else {
             window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                 WindowInsetsController controller = window.getInsetsController();
                 if (controller != null) {
-                    controller.show(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                    controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+                    controller.show(WindowInsets.Type.statusBars());
+                    controller.hide(WindowInsets.Type.navigationBars());
                 }
             }
         }
